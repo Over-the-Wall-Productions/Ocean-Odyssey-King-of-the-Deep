494b85a28a32c70cb57400891f7c9e5746cd0201
Zyanik <106106326+geperez8@users.noreply.github.com>
2024-02-26 22:28:20 -0600

    Sonar and bubbles

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -9,18 +9,25 @@ public class Shooting : MonoBehaviour
 
     public GameObject bulletPrefab;
 
+    public GameObject sonarPrefab;
+
     public float force = 20f;
 
     private float shotDelay = 0.5f;
     public float timeOfLastShot;
 
+    public float sonarDelay = 1f;
+    public float timeOfLastSonar;
+
     public UnityEvent<float> onReload;
 
     private bool canShoot = true; //new
 
+
+
     private void Update()
     {
-        
+
         if (!canShoot)
         {
             float currentDelay = shotDelay - (Time.time - timeOfLastShot);
@@ -38,6 +45,16 @@ public class Shooting : MonoBehaviour
             timeOfLastShot = Time.time;
             canShoot = false;
         }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            // Delay between shots
+            if (Time.time - timeOfLastSonar >= sonarDelay)
+            {
+                Sonar();
+                timeOfLastSonar = Time.time;
+            }
+
+        }
     }
 
     private void Shoot()
@@ -47,5 +64,14 @@ public class Shooting : MonoBehaviour
 
         // impulse adds foce impulse to RigidBody2D
         rb.AddForce(bulletSpawn.up * force, ForceMode2D.Impulse);
+    }
+
+    private void Sonar()
+    {
+        // Calculate the center of the current object
+        Vector3 center = transform.position;
+
+        // Instantiate the object at the center of the current object
+        Instantiate(sonarPrefab, center, Quaternion.identity);
     }
 }
