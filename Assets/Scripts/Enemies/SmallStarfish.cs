671474be34440d7dcbee8b2632317214d651a82d
Zyanik <106106326+geperez8@users.noreply.github.com>
2024-03-03 23:35:13 -0600

    Starfish Boss Implemented

---
+++ Assets/Scripts/Enemies/SmallStarfish.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class starfishSmall : MonoBehaviour
+{
+
+    [SerializeField] private float _damageAmount;
+
+    private float startTime;
+
+    private bool priorTo = true;
+
+    // health of starfish // takes 10 bullets
+    public int health = 20;
+
+    public Transform player; // Player's transform
+    public float speed = 5.0f; // Speed at which the object moves towards the player
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (priorTo && Time.time - startTime >= 2)
+        {
+            // Ensure this block runs only once
+            priorTo = false; // Disable this script to stop checking
+
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.velocity = Vector2.zero; // Stops linear movement
+                rb.angularVelocity = 0f; // Stops rotational movement
+            }
+
+            StartFollowing();
+        }
+
+        if (player != null)
+        {
+            // Move our position a step closer to the target.
+            float step = speed * Time.deltaTime; // Calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+        }
+    }
+
+    private void StartFollowing()
+    {
+
+        // Find the player by tag
+        GameObject playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerGameObject != null)
+        {
+            player = playerGameObject.transform;
+        }
+    }
+
+    //Add this function to handle collision with the bullet
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var playerBullet = collision.GetComponent<Bullet>();
+        if (collision.CompareTag("Bullet"))
+        {
+            TakeDamage(playerBullet.damage);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // a method that keeps track of damage and health for urchin
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+    //a method to drain player health on collision
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>())
+        {
+            var playerHealth = collision.GetComponent<HealthController>();
+            playerHealth.TakeDamage(_damageAmount);
+        }
+    }
+}
