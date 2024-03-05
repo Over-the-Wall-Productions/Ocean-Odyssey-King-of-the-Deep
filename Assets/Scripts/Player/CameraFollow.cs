1092f1c36e1d7df7bf84cc0ab70226df547a5d20
Zyanik <106106326+geperez8@users.noreply.github.com>
2024-03-05 11:47:56 -0600

    Fixed error output upon death

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -11,6 +11,10 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        if (target != null)
+        {
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
+        }
     }
 }
